a653b0d2efbba964d3a1106edd74b2d3cde98d8e
Andrey9019 <1234andrey5678@gmail.com>
2025-09-25 15:26:40 +0300

    add prebit.js

--- src/components/news/Article.tsx
+++ src/components/news/Article.tsx
@@ -8,32 +8,47 @@ export default function Article() {
 
   const { data: article, isLoading, error } = useArticleQuery(url);
 
-  if (isLoading) return <div className="container mx-auto px-4 py-8">Loading article...</div>;
+  if (isLoading)
+    return (
+      <div className="container mx-auto px-4 py-8">Loading article...</div>
+    );
   if (error)
-    return <div className="container mx-auto px-4 py-8 text-red-500">Error: {error?.message}</div>;
-  if (!article) return <div className="container mx-auto px-4 py-8">Article not found</div>;
+    return (
+      <div className="container mx-auto px-4 py-8 text-red-500">
+        Error: {error?.message}
+      </div>
+    );
+  if (!article)
+    return <div className="container mx-auto px-4 py-8">Article not found</div>;
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <div className="max-w-3xl mx-auto">
-        <div>
-          <h2 className="text-2xl font-bold">{article.title}</h2>
-          <p className="text-sm text-gray-500">{/* Author: {author} | Date: {date} */}</p>
-        </div>
-        <div>
-          <img
-            src={article.image}
-            alt={article.title}
-            loading="lazy"
-            className="w-full h-96 object-cover rounded-md mb-4"
-          />
+    <div className="flex flex-row">
+      <iframe id="ad-frame"></iframe>
 
-          <p className="text-gray-700 leading-relaxed">{article.content}</p>
-          <Link to="/" className="mt-6 inline-block">
-            <Button variant="outline">Back to feed</Button>
-          </Link>
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-3xl mx-auto">
+          <div>
+            <h2 className="text-2xl font-bold">{article.title}</h2>
+            <p className="text-sm text-gray-500">
+              {/* Author: {author} | Date: {date} */}
+            </p>
+          </div>
+          <div>
+            <img
+              src={article.image}
+              alt={article.title}
+              loading="lazy"
+              className="w-full h-96 object-cover rounded-md mb-4"
+            />
+
+            <p className="text-gray-700 leading-relaxed">{article.content}</p>
+            <Link to="/" className="mt-6 inline-block">
+              <Button variant="outline">Back to feed</Button>
+            </Link>
+          </div>
         </div>
       </div>
+      <iframe id="ad-frame-1"></iframe>
     </div>
   );
 }
